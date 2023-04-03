34af1f3242a929cba99f320b52a54a267c8e0a8f
Nabil Fadili <nhfadili@gmail.com>
2023-04-03 07:27:33 -0500

    update cr name to be unique (#3)

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
-# Container registries cannot be added to a DO project but the name will match
+# Container registries cannot be added to a DO project and their names must
+# be universally unique.
 resource "digitalocean_container_registry" "personal" {
-  name                   = "personal"
+  name                   = "nfadili_personal"
   subscription_tier_slug = "starter"
 }
