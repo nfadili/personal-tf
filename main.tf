6ef779b234b97ec7fdb104f1b150bcaae884326b
Nabil Fadili <nhfadili@gmail.com>
2023-04-03 07:33:44 -0500

    fix cr name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Container registries cannot be added to a DO project and their names must
 # be universally unique.
 resource "digitalocean_container_registry" "personal" {
-  name                   = "nfadili_personal"
+  name                   = "nfadili-personal"
   subscription_tier_slug = "starter"
 }
