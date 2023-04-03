c32ad0728ee3e83aaf70c06a7c3277c168129747
Nabil Fadili <nhfadili@gmail.com>
2023-04-03 06:59:07 -0500

    add project definition

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "digitalocean_kubernetes_cluster" "kubernetes_cluster" {
+resource "digitalocean_project" "personal" {
+  name        = "personal"
+  environment = "Development"
+  resources   = [digitalocean_kubernetes_cluster.personal_cluster.urn]
+}
+
+resource "digitalocean_kubernetes_cluster" "personal_cluster" {
   name    = "personal"
   region  = "nyc1"
   version = "1.26.3-do.0"
