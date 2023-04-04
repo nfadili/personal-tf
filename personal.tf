137ad4c13044d422b237d5e884c071fb199cfe4b
Nabil Fadili <nhfadili@gmail.com>
2023-04-04 10:04:20 -0500

    Add db cluster and db, add comments (#4)
    * add db cluster and db, add comments
    
    * rm cycle
    
    * fix name

--- personal.tf
+++ personal.tf
@@ -1,12 +1,16 @@
+#####################################
+# Shared/core resources
+#####################################
 resource "digitalocean_project" "personal" {
   name        = "personal"
   environment = "Development"
   resources = [
-    digitalocean_kubernetes_cluster.personal_cluster.urn
+    digitalocean_kubernetes_cluster.personal_kubernetes_cluster.urn,
+    digitalocean_database_cluster.personal_database_cluster.urn
   ]
 }
 
-resource "digitalocean_kubernetes_cluster" "personal_cluster" {
+resource "digitalocean_kubernetes_cluster" "personal_kubernetes_cluster" {
   name    = "personal"
   region  = "nyc1"
   version = "1.26.3-do.0"
@@ -16,4 +20,21 @@ resource "digitalocean_kubernetes_cluster" "personal_cluster" {
     size       = "s-1vcpu-2gb"
     node_count = 1
   }
+}
+
+resource "digitalocean_database_cluster" "personal_database_cluster" {
+  name       = "personal"
+  engine     = "pg"
+  version    = "15"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+}
+
+#####################################
+# Albumranker resources 
+#####################################
+resource "digitalocean_database_db" "personal_database_albumranker" {
+  cluster_id = digitalocean_database_cluster.personal_database_cluster.id
+  name       = "albumranker"
 }
