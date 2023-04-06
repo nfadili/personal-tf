8809f9426f2773426e37d2dbdcbf1d860dfadfd4
Nabil Fadili <nhfadili@gmail.com>
2023-04-06 13:26:12 -0500

    Add load balancer and domains for albumranker (#5)
    * add load balancer and domains for albumranker
    
    * add ingress and cert-manager resources, configure kubernetes and helm providers
    
    * maybe fix ingress errors with corect metadata access
    
    * fix depends on
    
    * rm issuer manifest resource

--- personal.tf
+++ personal.tf
@@ -1,12 +1,13 @@
 #####################################
-# Shared/core resources
+# Shared/core Digital Ocean resources
 #####################################
 resource "digitalocean_project" "personal" {
   name        = "personal"
   environment = "Development"
   resources = [
     digitalocean_kubernetes_cluster.personal_kubernetes_cluster.urn,
-    digitalocean_database_cluster.personal_database_cluster.urn
+    digitalocean_database_cluster.personal_database_cluster.urn,
+    digitalocean_loadbalancer.personal_kubernetes_ingress_loadbalancer.urn
   ]
 }
 
@@ -31,10 +32,143 @@ resource "digitalocean_database_cluster" "personal_database_cluster" {
   node_count = 1
 }
 
+resource "digitalocean_loadbalancer" "personal_kubernetes_ingress_loadbalancer" {
+  name      = "personal"
+  region    = "nyc1"
+  size      = "lb-small"
+  algorithm = "round_robin"
+
+  forwarding_rule {
+    entry_port      = 80
+    entry_protocol  = "http"
+    target_port     = 80
+    target_protocol = "http"
+
+  }
+
+  # Configuration is managed in kubernetes so we ignore all changes after initial creation
+  lifecycle {
+    ignore_changes = [
+      forwarding_rule,
+    ]
+  }
+
+  depends_on = [
+    digitalocean_kubernetes_cluster.personal_kubernetes_cluster
+  ]
+}
+
+#####################################
+# Cluster core resources 
+#####################################
+resource "helm_release" "personal_nginx_ingress_controller" {
+  name       = "nginx-ingress-controller"
+  namespace  = "default"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "service.annotations.kubernetes\\.digitalocean\\.com/load-balancer-id"
+    value = digitalocean_loadbalancer.personal_kubernetes_ingress_loadbalancer.id
+  }
+  depends_on = [
+    digitalocean_loadbalancer.personal_kubernetes_ingress_loadbalancer,
+  ]
+}
+
+
+
+resource "helm_release" "personal_certmanager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v1.11.0"
+  namespace  = "kube-system"
+  timeout    = 120
+  depends_on = [
+    helm_release.personal_nginx_ingress_controller,
+  ]
+  set {
+    name  = "createCustomResource"
+    value = "true"
+  }
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
+
+# TODO: Figure out if we should apply the ClusterIssuer manifest here in terraform or alongside other kubernetes resources
+# https://stackoverflow.com/questions/68511476/setup-letsencrypt-clusterissuer-with-terraform
+# - Applying here would require the cluster to already be created before applying
+# - Applying with other kubernetes resources put the onus on the app deployment to manager cert issuing
+
+
 #####################################
 # Albumranker resources 
 #####################################
+resource "kubernetes_namespace" "albumranker_namespace" {
+  metadata {
+    name = "albumranker"
+  }
+}
+
+resource "digitalocean_domain" "albumranker_domain" {
+  name = "albumranker.com"
+}
+
+resource "digitalocean_record" "albumranker_a_record" {
+  domain = "albumranker.com"
+  type   = "A"
+  ttl    = 60
+  name   = "@"
+  value  = digitalocean_loadbalancer.personal_kubernetes_ingress_loadbalancer.ip
+  depends_on = [
+    digitalocean_domain.albumranker_domain,
+    kubernetes_ingress.albumranker_ingress
+  ]
+}
+
+resource "kubernetes_ingress" "albumranker_ingress" {
+  depends_on = [
+    helm_release.personal_nginx_ingress_controller,
+  ]
+  metadata {
+    name      = "albumranker-ingress"
+    namespace = kubernetes_namespace.albumranker_namespace.metadata[0].name
+    annotations = {
+      "kubernetes.io/ingress.class"          = "nginx"
+      "ingress.kubernetes.io/rewrite-target" = "/"
+      "cert-manager.io/cluster-issuer"       = "letsencrypt-production"
+    }
+  }
+  spec {
+    rule {
+      host = "albumranker.com"
+      http {
+        path {
+          path = "/"
+          backend {
+            service_name = "albumranker-com-service"
+            service_port = 80
+          }
+        }
+      }
+    }
+    tls {
+      secret_name = "albumranker-com-tls"
+      hosts = [
+        "albumranker.com"
+      ]
+    }
+  }
+}
+
 resource "digitalocean_database_db" "personal_database_albumranker" {
   cluster_id = digitalocean_database_cluster.personal_database_cluster.id
   name       = "albumranker"
 }
+
--- terraform.tf
+++ terraform.tf
@@ -14,6 +14,14 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.1"
+    }
   }
 
   required_version = ">= 0.14.0"
@@ -25,4 +33,26 @@ variable "do_token" {}
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
+}
+
+# Configure the kubernetes provider with credentials for the personal kubernetes cluster
+provider "kubernetes" {
+  host  = digitalocean_kubernetes_cluster.personal_kubernetes_cluster.endpoint
+  token = digitalocean_kubernetes_cluster.personal_kubernetes_cluster.kube_config[0].token
+
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.personal_kubernetes_cluster.kube_config[0].cluster_ca_certificate
+  )
+}
+
+# Configure the helm provider with credentaisl for the personal kubernetes cluster
+provider "helm" {
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.personal_kubernetes_cluster.endpoint
+    token = digitalocean_kubernetes_cluster.personal_kubernetes_cluster.kube_config[0].token
+
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.personal_kubernetes_cluster.kube_config[0].cluster_ca_certificate
+    )
+  }
 }
