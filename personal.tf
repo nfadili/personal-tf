5ebe7a17c49eb68c888f2024b1a8509ff28b2c65
Nabil Fadili <nhfadili@gmail.com>
2023-04-06 12:13:42 -0500

    add load balancer and domains for albumranker

--- personal.tf
+++ personal.tf
@@ -6,7 +6,8 @@ resource "digitalocean_project" "personal" {
   environment = "Development"
   resources = [
     digitalocean_kubernetes_cluster.personal_kubernetes_cluster.urn,
-    digitalocean_database_cluster.personal_database_cluster.urn
+    digitalocean_database_cluster.personal_database_cluster.urn,
+    digitalocean_loadbalancer.personal_kubernetes_ingress_loadbalancer.urn
   ]
 }
 
@@ -31,10 +32,50 @@ resource "digitalocean_database_cluster" "personal_database_cluster" {
   node_count = 1
 }
 
+resource "digitalocean_loadbalancer" "personal_kubernetes_ingress_loadbalancer" {
+  name      = "personal"
+  region    = "nyc1"
+  size      = "lb-small"
+  algorithm = "round_robin"
+
+  forwarding_rule {
+    entry_port      = 80
+    entry_protocol  = "http"
+    target_port     = 80
+    target_protocol = "http"
+
+  }
+
+  # Configuration is managed in kubernetes so we ignore all changes after initial creation
+  lifecycle {
+    ignore_changes = [
+      forwarding_rule,
+    ]
+  }
+
+}
+
 #####################################
 # Albumranker resources 
 #####################################
+resource "digitalocean_domain" "albumranker_domain" {
+  name = "albumranker.com"
+}
+
+resource "digitalocean_record" "albumranker_a_record" {
+  domain = "albumranker.com"
+  type   = "A"
+  ttl    = 60
+  name   = "@"
+  value  = digitalocean_loadbalancer.personal_kubernetes_ingress_loadbalancer.ip
+  depends_on = [
+    digitalocean_domain.albumranker_domain,
+    kubernetes_ingress.personal_kubernetes_ingress_loadbalancer
+  ]
+}
+
 resource "digitalocean_database_db" "personal_database_albumranker" {
   cluster_id = digitalocean_database_cluster.personal_database_cluster.id
   name       = "albumranker"
 }
+
